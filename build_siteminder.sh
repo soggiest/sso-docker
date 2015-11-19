4e769a7134d4d565d250ae7306118c6c8ad24cc7
Nick Lane <nlane@redhat.com>
2015-11-19 11:34:25 -0800

    Update build_siteminder.sh

--- build_siteminder.sh
+++ build_siteminder.sh
@@ -2,13 +2,13 @@
 #
 #Runs the Siteminder scripts for a container
 
-chmod 755 /root/ca-wa-12.0-sp3-cr012-linux.bin
+chmod 755 /root/ca-wa-12.5-cr02-rhas64.bin
 
-cat <EOF > /root/siteminder_installer.properties 
+cat <<EOF > /root/siteminder_installer.properties 
 USER_INSTALL_DIR=/root/siteminder/webagent/r12.5
 USER_SHORTCUTS=/root
 EOF
 
 mkdir -p /root/siteminder/webagent/r12.5
 
-/tmp/ca-wa-12.0-sp3-cr012-linux.bin -f /root/siteminder_installer.properties -i silent >/dev/null 2>&1
+/tmp/ca-wa-12.5-cr02-rhas64.bin -f /root/siteminder_installer.properties -i silent >/dev/null 2>&1
