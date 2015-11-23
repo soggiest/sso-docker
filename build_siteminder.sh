f1daf917a583a501ef433bf98315f57c44093430
Nick Lane <nlane@redhat.com>
2015-11-23 15:08:35 -0800

    Update build_siteminder.sh

--- build_siteminder.sh
+++ build_siteminder.sh
@@ -11,4 +11,6 @@ EOF
 
 mkdir -p /root/siteminder/webagent/r12.5
 
+chmod -R 755 /root/siteminder/webagent/r12.5
+
 /root/ca-wa-12.5-cr02-rhas64.bin -f /root/siteminder_installer.properties -i silent >/dev/null 2>&1
